a82e992bb63f22e2faf0abe44626bfc0cff1761d
Trevor Greenleaf <info@trevorgreenleaf.com>
2020-05-17 14:33:35 -0700

    Updated some more of the backgrounds

--- src/components/Brief.vue
+++ src/components/Brief.vue
@@ -2,25 +2,30 @@
   <div>
 
 
-<div class="max-w-screen-md bg-no-repeat mx-auto" >
+<div class=" max-w-screen-md bg-no-repeat mx-auto  mt-24">
 
-  <div class="mt-8">
-    <h2 class="font-bold text-5xl ">Brief</h2>
-    <p class="mb-8 text-orange">All times on PDT.</p>
-  </div>
 
-  <p class="mb-4">During May 23-30, 2020 of the summer term, the Hackathon Club is organizing a 1-week remote design challenge in collaboration with Designmatters and Entrepreneurship and Professional Practice. </p>
-   
-  <p class="mb-4">A critical component of this design challenge is the research phase and the support provided by experts. We will be bringing in experts for question-answer sessions where they will share their knowledge and experiences on the specified topics. The one-week design challenge will give you the opportunity to work differently and experiment with different roles. We encourage you to take risks that you normally would not in class or at work. </p>
-   
-  <p class="mb-4">The goal of the challenge is to develop project ideas, potential solutions, and ongoing cross-disciplinary teams that can address our radically changing world and prepare for future challenges caused by health crises and other global disruptions. Put your expertise to work, delve deeper into the issues people are facing today and make a difference by catalyzing the solutions needed now and in the future.</p>
-   
-  <p class="mb-4">The top three topics that will be addressed in this design challenge include remote collaboration, health, and education. The focus of this Design Challenge is not only to design for COVID-19, but also for future global catastrophes.</p>
-   
-  <p class="mb-4">Participants will be organized into teams with the same topic interest and guided in collaboration so that all who join have the opportunity to work with other majors to ensure diverse methods and backgrounds.</p>
-   
-  <p class="mb-4">The big picture: We are working with ArtCenter to make it possible for the winning teams to continue their projects in later terms in alignment with degree audits. These groups will have the opportunity to connect with students in engineering and medical programs at Caltech and UCLA, along with mentors from the healthcare, education, and technology sectors. Successful teams will be encouraged to apply for business plan competitions, project funding from VentureWell, and other pre-seed investors. 
-  </p>
+
+
+    <div class="mt-8 text-center">
+      <h2 class="font-bold text-4xl ">Brief</h2>
+      <p class="mb-8 text-orange">All times on PDT.</p>
+    </div>
+
+    <p class="mb-4">During May 23-30, 2020 of the summer term, the Hackathon Club is organizing a 1-week remote design challenge in collaboration with Designmatters and Entrepreneurship and Professional Practice. </p>
+     
+    <p class="mb-4">A critical component of this design challenge is the research phase and the support provided by experts. We will be bringing in experts for question-answer sessions where they will share their knowledge and experiences on the specified topics. The one-week design challenge will give you the opportunity to work differently and experiment with different roles. We encourage you to take risks that you normally would not in class or at work. 
+     <img  class="float-left w-2/3 " src="/images/bg-4.svg" alt=""></p>
+     
+   <p class="mb-4">The goal of the challenge is to develop project ideas, potential solutions, and ongoing cross-disciplinary teams that can address our radically changing world and prepare for future challenges caused by health crises and other global disruptions. Put your expertise to work, delve deeper into the issues people are facing today and make a difference by catalyzing the solutions needed now and in the future.</p>
+     
+    <p class="mb-4">The top three topics that will be addressed in this design challenge include remote collaboration, health, and education. The focus of this Design Challenge is not only to design for COVID-19, but also for future global catastrophes.</p>
+     
+    <p class="mb-4">Participants will be organized into teams with the same topic interest and guided in collaboration so that all who join have the opportunity to work with other majors to ensure diverse methods and backgrounds.</p>
+     
+    <p class="mb-4">The big picture: We are working with ArtCenter to make it possible for the winning teams to continue their projects in later terms in alignment with degree audits. These groups will have the opportunity to connect with students in engineering and medical programs at Caltech and UCLA, along with mentors from the healthcare, education, and technology sectors. Successful teams will be encouraged to apply for business plan competitions, project funding from VentureWell, and other pre-seed investors. 
+    </p>
+
 
 
 </div>
