69d9706210a3c03d59d5bf07aa4bacb23bbcd948
Trevor Greenleaf <info@trevorgreenleaf.com>
2020-05-17 13:21:20 -0700

    Formated the tables

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,113 +1,117 @@
 <template>
-  <div class="mt-24 flex flex-col justify-center justify-center text-center">
+  <div class="mt-24 flex flex-col justify-center justify-center max-w-screen-sm mx-auto ">
 	
-	<h2 class="font-bold text-5xl text-center">Schedule</h2>
+	<div class="text-center">
+		<h2 class="font-bold text-5xl ">Schedule</h2>
+		<p class="">All times listed in PDT.</p>
+	</div>
 
-<h3 class="font-bold text-2xl">Saturday, May 23rd, 2020</h3>
-<table class="table-auto">
+
+<h3 class="font-bold text-2xl mb-4 mt-12">Saturday, May 23rd, 2020</h3>
+<table class="table-auto text-left">
   <thead>
     <tr>
-      <th class="px-4 py-2">Name</th>
+      <th class="px-4 py-2">Event</th>
       <th class="px-4 py-2">Time</th>
    </tr>
   </thead>
   <tbody>
     <tr>
-      <td class="border px-4 py-2">Kick-off meeting</td>
-      <td class="border px-4 py-2">10:00-10:30am</td>
+      <td class="border-b px-4 py-2">Kick-off meeting</td>
+      <td class="border-b px-4 py-2">10:00-10:30am</td>
     </tr>
     
     <tr>
-      <td class="border px-4 py-2">Lightning Talks</td>
-      <td class="border px-4 py-2">10:30am – 12:40pm</td>
+      <td class="border-b px-4 py-2">Lightning Talks</td>
+      <td class="border-b px-4 py-2">10:30am – 12:40pm</td>
     </tr>
 
      <tr>
-      <td class="border px-4 py-2">Lunch</td>
-      <td class="border px-4 py-2"></td>
+      <td class="border-b px-4 py-2">Lunch</td>
+      <td class="border-b px-4 py-2"></td>
     </tr>
 
      <tr>
-      <td class="border px-4 py-2">Q&A</td>
-      <td class="border px-4 py-2">1:30 – 2:15pm</td>
+      <td class="border-b px-4 py-2">Q&A</td>
+      <td class="border-b px-4 py-2">1:30 – 2:15pm</td>
     </tr>
 
      <tr>
-      <td class="border px-4 py-2">Adobe Workshop and Talk</td>
-      <td class="border px-4 py-2">2:30 – 3:30pm</td>
+      <td class="border-b px-4 py-2">Adobe Workshop and Talk</td>
+      <td class="border-b px-4 py-2">2:30 – 3:30pm</td>
     </tr>
   </tbody>
 </table>
 
 
 
-<h3 class="font-bold text-2xl">Sunday, May 23rd, 2020</h3>
-<table class="table-auto">
+<h3 class="font-bold text-2xl mb-4 mt-12">Sunday, May 23rd, 2020</h3>
+<table class="table-auto text-left">
   <thead>
     <tr>
-      <th class="px-4 py-2">Name</th>
+      <th class="px-4 py-2">Event</th>
       <th class="px-4 py-2">Time</th>
    </tr>
   </thead>
   <tbody>
     <tr>
-      <td class="border px-4 py-2">Talks & Workshops</td>
-      <td class="border px-4 py-2">10:00-12:30am</td>
+      <td class="border-b px-4 py-2">Talks & Workshops</td>
+      <td class="border-b px-4 py-2">10:00-12:30am</td>
     </tr>
    
   </tbody>
 </table>
 
-<h3 class="font-bold text-2xl">Monday, May 25th through Friday, May 29th</h3>
-<table class="table-auto">
+<h3 class="font-bold text-2xl mb-4 mt-12">Monday, May 25th through Friday, May 29th</h3>
+<table class="table-auto text-left">
   <thead>
     <tr>
-      <th class="px-4 py-2">Name</th>
+      <th class="px-4 py-2">Event</th>
       <th class="px-4 py-2">Time</th>
    </tr>
   </thead>
   <tbody>
     <tr>
-      <td class="border px-4 py-2">Evening workshops (coming soon)</td>
-      <td class="border px-4 py-2">TBA</td>
+      <td class="border-b px-4 py-2">Evening workshops (coming soon)</td>
+      <td class="border-b px-4 py-2">TBA</td>
     </tr>
 
     <tr>
-      <td class="border px-4 py-2">Pitch Workshop</td>
-      <td class="border px-4 py-2">Friday Night Time TBA</td>
+      <td class="border-b px-4 py-2">Pitch Workshop</td>
+      <td class="border-b px-4 py-2">Friday Night Time TBA</td>
     </tr>
    
   </tbody>
 </table>
 
-<h3 class="font-bold text-2xl">Saturday, May 30, 2020</h3>
+<h3 class="font-bold text-2xl mb-4 mt-12">Saturday, May 30, 2020</h3>
 <p>All presentations need to be submitted immediately after your pitch to be judged.</p>
-<table class="table-auto">
+<table class="table-auto text-left">
   <thead>
     <tr>
-      <th class="px-4 py-2">Name</th>
+      <th class="px-4 py-2">Event</th>
       <th class="px-4 py-2">Time</th>
    </tr>
   </thead>
   <tbody>
     <tr>
-      <td class="border px-4 py-2">Student Present Pitches A - E</td>
-      <td class="border px-4 py-2">11:00am – 12:30pm</td>
+      <td class="border-b px-4 py-2">Student Present Pitches A - E</td>
+      <td class="border-b px-4 py-2">11:00am – 12:30pm</td>
     </tr>
 
     <tr>
-      <td class="border px-4 py-2">Lunch</td>
-      <td class="border px-4 py-2"></td>
+      <td class="border-b px-4 py-2">Lunch</td>
+      <td class="border-b px-4 py-2"></td>
     </tr>
 
     <tr>
-      <td class="border px-4 py-2">Students Present Pitches F-J</td>
-      <td class="border px-4 py-2">1:20 – 2:30pm</td>
+      <td class="border-b px-4 py-2">Students Present Pitches F-J</td>
+      <td class="border-b px-4 py-2">1:20 – 2:30pm</td>
     </tr>
 
     <tr>
-      <td class="border px-4 py-2">Closing Ceremony (Winners announced)</td>
-      <td class="border px-4 py-2">4:00 – 5:00pm</td>
+      <td class="border-b px-4 py-2">Closing Ceremony (Winners announced)</td>
+      <td class="border-b px-4 py-2">4:00 – 5:00pm</td>
     </tr>
    
   </tbody>
