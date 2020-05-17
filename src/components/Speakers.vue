eea557cc9829c893bbfc38ffef4e308647f26350
Trevor Greenleaf <info@trevorgreenleaf.com>
2020-05-17 13:55:35 -0700

    Added speaker images

--- src/components/Speakers.vue
+++ src/components/Speakers.vue
@@ -27,7 +27,7 @@
 		<div class="w-1/3 flex flex-col items-center mb-12 ">
 			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
-					<img src="/images/speakers/c_airmet.jfif" alt="person name">
+					<img src="/images/speakers/k_cooper.jfif" alt="person name">
 				</div>
 				<p class="font-bold text-xl">Krystal Cooper <span class=" text-sm block font-normal text-gray-500">Technical Producer, Unity</span></p>
 			</a>
@@ -36,7 +36,7 @@
 		<div class="w-1/3 flex flex-col items-center mb-12 ">
 			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
-					<img src="/images/speakers/c_airmet.jfif" alt="person name">
+					<img src="/images/speakers/l_shields.jfif" alt="person name">
 				</div>
 				<p class="font-bold text-xl">Lance Shields <span class=" text-sm block font-normal text-gray-500">Director of International Design, Adobe Team</span></p>
 			</a>
@@ -54,27 +54,16 @@
 		<div class="w-1/3 flex flex-col items-center mb-12 ">
 			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
-					<img src="/images/speakers/c_airmet.jfif" alt="person name">
+					<img src="/images/speakers/m_estano.jfif" alt="person name">
 				</div>
 				<p class="font-bold text-xl">Mike Estano <span class=" text-sm block font-normal text-gray-500">UX Strategy/Creative Director</span></p>
 			</a>
 		</div>
 
-
 		<div class="w-1/3 flex flex-col items-center mb-12 ">
 			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
-					<img src="/images/speakers/c_airmet.jfif" alt="person name">
-				</div>
-				<p class="font-bold text-xl">Nardo Manaloto <span class=" text-sm block font-normal text-gray-500">CEO, Catalaize</span></p>
-			</a>
-		</div>
-
-
-		<div class="w-1/3 flex flex-col items-center mb-12 ">
-			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
-				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
-					<img src="/images/speakers/c_airmet.jfif" alt="person name">
+					<img src="/images/speakers/n_manaloto.jfif" alt="person name">
 				</div>
 				<p class="font-bold text-xl">Nardo Manaloto <span class=" text-sm block font-normal text-gray-500">CEO, Catalaize</span></p>
 			</a>
@@ -83,7 +72,7 @@
 		<div class="w-1/3 flex flex-col items-center mb-12 ">
 			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
-					<img src="/images/speakers/c_airmet.jfif" alt="person name">
+					<img src="/images/speakers/r_nock.jfif" alt="person name">
 				</div>
 				<p class="font-bold text-xl">Robbie Nock <span class=" text-sm block font-normal text-gray-500">Design, Innovation & Creative Leadership, ArtCenter</span></p>
 			</a>
@@ -92,7 +81,7 @@
 		<div class="w-1/3 flex flex-col items-center mb-12 ">
 			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
-					<img src="/images/speakers/c_airmet.jfif" alt="person name">
+					<img src="/images/speakers/skhan.jfif" alt="person name">
 				</div>
 				<p class="font-bold text-xl">Sofia Mohammad Khan<span class=" text-sm block font-normal text-gray-500">UX Design Director and Corporate Trainer, SK+CO</span></p>
 			</a>
@@ -102,9 +91,18 @@
 		<div class="w-1/3 flex flex-col items-center mb-12 ">
 			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
-					<img src="/images/speakers/c_airmet.jfif" alt="person name">
+					<img src="/images/speakers/s_yanchinski.jfif" alt="person name">
 				</div>
 				<p class="font-bold text-xl">Stephanie Yanchinski<span class=" text-sm block font-normal text-gray-500">Director of Entrepreneurship, Resnick Sustainability Institute at Caltech</span></p>
+			</a>
+		</div>
+
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
+				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
+					<img src="/images/speakers/tknight.jfif" alt="person name">
+				</div>
+				<p class="font-bold text-xl">Tara Knight<span class=" text-sm block font-normal text-gray-500">Strategic Development Manager, Creative Cloud Design</span></p>
 			</a>
 		</div>
 		
