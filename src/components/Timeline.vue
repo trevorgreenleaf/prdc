2070a9afb9bbef65ef7a7aff764031813766fefc
Trevor Greenleaf <info@trevorgreenleaf.com>
2020-05-17 14:08:53 -0700

    Updated border color

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="mt-24 flex flex-col justify-center justify-center max-w-screen-sm mx-auto ">
-	
-	<div class="text-center">
-		<h2 class="font-bold text-5xl ">Schedule</h2>
-		<p class="">All times listed in PDT.</p>
-	</div>
+  
+  <div class="text-center">
+    <h2 class="font-bold text-5xl ">Schedule</h2>
+    <p class="">All times listed in PDT.</p>
+  </div>
 
 
 <h3 class="font-bold text-2xl mb-4 mt-12">Saturday, May 23rd, 2020</h3>
@@ -17,28 +17,28 @@
   </thead>
   <tbody>
     <tr>
-      <td class="border-b px-4 py-2">Kick-off meeting</td>
-      <td class="border-b px-4 py-2">10:00-10:30am</td>
+      <td class="border-b border-gray-300 px-4 py-2">Kick-off meeting</td>
+      <td class="border-b border-gray-300 px-4 py-2">10:00-10:30am</td>
     </tr>
     
     <tr>
-      <td class="border-b px-4 py-2">Lightning Talks</td>
-      <td class="border-b px-4 py-2">10:30am – 12:40pm</td>
+      <td class="border-b border-gray-300 px-4 py-2">Lightning Talks</td>
+      <td class="border-b border-gray-300 px-4 py-2">10:30am – 12:40pm</td>
     </tr>
 
      <tr>
-      <td class="border-b px-4 py-2">Lunch</td>
-      <td class="border-b px-4 py-2"></td>
+      <td class="border-b border-gray-300 px-4 py-2">Lunch</td>
+      <td class="border-b border-gray-300 px-4 py-2"></td>
     </tr>
 
      <tr>
-      <td class="border-b px-4 py-2">Q&A</td>
-      <td class="border-b px-4 py-2">1:30 – 2:15pm</td>
+      <td class="border-b border-gray-300 px-4 py-2">Q&A</td>
+      <td class="border-b border-gray-300 px-4 py-2">1:30 – 2:15pm</td>
     </tr>
 
      <tr>
-      <td class="border-b px-4 py-2">Adobe Workshop and Talk</td>
-      <td class="border-b px-4 py-2">2:30 – 3:30pm</td>
+      <td class="border-b border-gray-300 px-4 py-2">Adobe Workshop and Talk</td>
+      <td class="border-b border-gray-300 px-4 py-2">2:30 – 3:30pm</td>
     </tr>
   </tbody>
 </table>
@@ -55,8 +55,8 @@
   </thead>
   <tbody>
     <tr>
-      <td class="border-b px-4 py-2">Talks & Workshops</td>
-      <td class="border-b px-4 py-2">10:00-12:30am</td>
+      <td class="border-b border-gray-300 px-4 py-2">Talks & Workshops</td>
+      <td class="border-b border-gray-300 px-4 py-2">10:00-12:30am</td>
     </tr>
    
   </tbody>
@@ -72,13 +72,13 @@
   </thead>
   <tbody>
     <tr>
-      <td class="border-b px-4 py-2">Evening workshops (coming soon)</td>
-      <td class="border-b px-4 py-2">TBA</td>
+      <td class="border-b border-gray-300 px-4 py-2">Evening workshops (coming soon)</td>
+      <td class="border-b border-gray-300 px-4 py-2">TBA</td>
     </tr>
 
     <tr>
-      <td class="border-b px-4 py-2">Pitch Workshop</td>
-      <td class="border-b px-4 py-2">Friday Night Time TBA</td>
+      <td class="border-b border-gray-300 px-4 py-2">Pitch Workshop</td>
+      <td class="border-b border-gray-300 px-4 py-2">Friday Night Time TBA</td>
     </tr>
    
   </tbody>
@@ -95,29 +95,29 @@
   </thead>
   <tbody>
     <tr>
-      <td class="border-b px-4 py-2">Student Present Pitches A - E</td>
-      <td class="border-b px-4 py-2">11:00am – 12:30pm</td>
+      <td class="border-b border-gray-300 px-4 py-2">Student Present Pitches A - E</td>
+      <td class="border-b border-gray-300 px-4 py-2">11:00am – 12:30pm</td>
     </tr>
 
     <tr>
-      <td class="border-b px-4 py-2">Lunch</td>
-      <td class="border-b px-4 py-2"></td>
+      <td class="border-b border-gray-300 px-4 py-2">Lunch</td>
+      <td class="border-b border-gray-300 px-4 py-2"></td>
     </tr>
 
     <tr>
-      <td class="border-b px-4 py-2">Students Present Pitches F-J</td>
-      <td class="border-b px-4 py-2">1:20 – 2:30pm</td>
+      <td class="border-b border-gray-300 px-4 py-2">Students Present Pitches F-J</td>
+      <td class="border-b border-gray-300 px-4 py-2">1:20 – 2:30pm</td>
     </tr>
 
     <tr>
-      <td class="border-b px-4 py-2">Closing Ceremony (Winners announced)</td>
-      <td class="border-b px-4 py-2">4:00 – 5:00pm</td>
+      <td class="border-b border-gray-300 px-4 py-2">Closing Ceremony (Winners announced)</td>
+      <td class="border-b border-gray-300 px-4 py-2">4:00 – 5:00pm</td>
     </tr>
    
   </tbody>
 </table>
 
-	
+  
 
 
 
