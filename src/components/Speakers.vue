b43ee60abca793e7c5266893fb82576ddacff466
Trevor Greenleaf <info@trevorgreenleaf.com>
2020-05-17 13:51:15 -0700

    Updated people

--- src/components/Speakers.vue
+++ src/components/Speakers.vue
@@ -1,82 +1,113 @@
 <template>
-  <div class="mt-24 flex flex-col justify-center justify-center text-center">
+  <div class="mt-24 flex flex-col items-center mb-12 justify-center text-center">
 
 	
 	<!-- <p class="text-orange"></h2> -->
-	<h2 class="font-bold text-5xl text-center">Speakers</h2>
+	<h2 class="font-bold text-5xl text-center mb-8">Speakers</h2>
 	
 	<div class="flex flex-wrap max-w-screen-md mx-auto">
-		<div class="w-1/3 flex flex-col justify-center ">
-			<a target="_blank" class="text-center flex flex-col" href="somelink">
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
 					<img src="/images/speakers/c_airmet.jfif" alt="person name">
 				</div>
-				<p> Some name</p>
+				<p class="font-bold text-xl">Adrian Ang <span class=" text-sm block font-normal text-gray-500">Nurse, Cedars Sinai</span></p>
 			</a>
 		</div>
 
-		<div class="w-1/3 flex flex-col justify-center ">
-			<a target="_blank" class="text-center flex flex-col" href="somelink">
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
 					<img src="/images/speakers/c_airmet.jfif" alt="person name">
 				</div>
-				<p> Some name</p>
+				<p class="font-bold text-xl">Chase Airmet <span class=" text-sm block font-normal text-gray-500">Senior Product Designer, Ascension</span></p>
 			</a>
 		</div>
 
-		<div class="w-1/3 flex flex-col justify-center ">
-			<a target="_blank" class="text-center flex flex-col" href="somelink">
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
 					<img src="/images/speakers/c_airmet.jfif" alt="person name">
 				</div>
-				<p> Some name</p>
+				<p class="font-bold text-xl">Krystal Cooper <span class=" text-sm block font-normal text-gray-500">Technical Producer, Unity</span></p>
 			</a>
 		</div>
 
-		<div class="w-1/3 flex flex-col justify-center ">
-			<a target="_blank" class="text-center flex flex-col" href="somelink">
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
 					<img src="/images/speakers/c_airmet.jfif" alt="person name">
 				</div>
-				<p> Some name</p>
+				<p class="font-bold text-xl">Lance Shields <span class=" text-sm block font-normal text-gray-500">Director of International Design, Adobe Team</span></p>
 			</a>
 		</div>
 
-		<div class="w-1/3 flex flex-col justify-center ">
-			<a target="_blank" class="text-center flex flex-col" href="somelink">
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
 					<img src="/images/speakers/c_airmet.jfif" alt="person name">
 				</div>
-				<p> Some name</p>
+				<p class="font-bold text-xl">Majenta Strongheart <span class=" text-sm block font-normal text-gray-500">Head of Design and Partnerships, DesignLab at Supplyframe</span></p>
 			</a>
 		</div>
 
-		<div class="w-1/3 flex flex-col justify-center ">
-			<a target="_blank" class="text-center flex flex-col" href="somelink">
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
 					<img src="/images/speakers/c_airmet.jfif" alt="person name">
 				</div>
-				<p> Some name</p>
+				<p class="font-bold text-xl">Mike Estano <span class=" text-sm block font-normal text-gray-500">UX Strategy/Creative Director</span></p>
 			</a>
 		</div>
 
-		<div class="w-1/3 flex flex-col justify-center ">
-			<a target="_blank" class="text-center flex flex-col" href="somelink">
+
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
 					<img src="/images/speakers/c_airmet.jfif" alt="person name">
 				</div>
-				<p> Some name</p>
+				<p class="font-bold text-xl">Nardo Manaloto <span class=" text-sm block font-normal text-gray-500">CEO, Catalaize</span></p>
 			</a>
 		</div>
 
-		<div class="w-1/3 flex flex-col justify-center ">
-			<a target="_blank" class="text-center flex flex-col" href="somelink">
+
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
 				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
 					<img src="/images/speakers/c_airmet.jfif" alt="person name">
 				</div>
-				<p> Some name</p>
+				<p class="font-bold text-xl">Nardo Manaloto <span class=" text-sm block font-normal text-gray-500">CEO, Catalaize</span></p>
 			</a>
 		</div>
+
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
+				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
+					<img src="/images/speakers/c_airmet.jfif" alt="person name">
+				</div>
+				<p class="font-bold text-xl">Robbie Nock <span class=" text-sm block font-normal text-gray-500">Design, Innovation & Creative Leadership, ArtCenter</span></p>
+			</a>
+		</div>
+
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
+				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
+					<img src="/images/speakers/c_airmet.jfif" alt="person name">
+				</div>
+				<p class="font-bold text-xl">Sofia Mohammad Khan<span class=" text-sm block font-normal text-gray-500">UX Design Director and Corporate Trainer, SK+CO</span></p>
+			</a>
+		</div>
+
+
+		<div class="w-1/3 flex flex-col items-center mb-12 ">
+			<a target="_blank" class="text-center flex flex-col items-center" href="somelink">
+				<div class="rounded-full border-4 border-black w-48 h-48 overflow-hidden">
+					<img src="/images/speakers/c_airmet.jfif" alt="person name">
+				</div>
+				<p class="font-bold text-xl">Stephanie Yanchinski<span class=" text-sm block font-normal text-gray-500">Director of Entrepreneurship, Resnick Sustainability Institute at Caltech</span></p>
+			</a>
+		</div>
+		
 	</div>
 
 
