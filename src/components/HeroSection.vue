764d4de386fb149c2842cbdda63dde128ff46e5d
ssmoon89 <65506334+ssmoon89@users.noreply.github.com>
2020-05-31 19:07:32 -0700

    winners update

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -7,16 +7,25 @@
           Pandemic Response <br> <span class="text-black">Design Challenge</span>
         </h1>
        <!-- <p class="mb-8 pt-4 text-orange">Registration closes at noon on 5/21/20, Sign up now!</p> -->
-        <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+        <!-- <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           We hope everyone had a safe and relaxing break. If you are ready to take on the challenge, ArtCenter’s "Hack"athon Club has put together a design challenge to address current and future disruptions. 
+        </p> -->
+        <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+          1st place: Connext
         </p>
-        <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center ">
+        <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+          2nd place tie between: Kandu and Playground
+        </p>
+        <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+          3rd place: Relay
+        </p>
+        <!-- <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center ">
           <div>
             <a href="#brief" v-scroll-to="'#brief'" class="w-full flex items-center justify-center px-8 py-3 text-base leading-6 font-bold rounded-full text-white bg-orange hover:bg-orange  md:py-4 md:text-lg md:px-10">
               Learn More
             </a>
           </div>
-        </div>
+        </div> -->
     </div>
 
     <div class="bg-black">
