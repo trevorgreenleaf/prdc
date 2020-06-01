769b4a85a645df360b73a4732fb07977d750b117
ssmoon89 <65506334+ssmoon89@users.noreply.github.com>
2020-05-31 22:51:29 -0700

    Update HeroSection.vue
    team update

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -14,7 +14,7 @@
           1st place: Connext
         </p>
         <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-          2nd place tie between: Kandu and Playground
+          2nd place tie between: Kando and Playground
         </p>
         <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           3rd place: Relay
